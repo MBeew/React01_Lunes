d1179566c40713cd430edfc000c8a2a309672d64
Emely <erueda10@cuc.edu.co>
2024-09-16 09:41:19 -0500

    Proyecto_React_01

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import milogo from "./assets/img/logoReact.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={milogo} alt="Logo de React" />
+        <h1>Hola Mundo!</h1>
+        <p>Este esmi primer proyecto en React</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
